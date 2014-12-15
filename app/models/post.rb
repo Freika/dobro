a68987e1da19b6ffdc5da31ab5ff1847b7e7a99a
Freika <iamfrey@gmail.com>
2014-12-15 16:17:31 +0600

    Added: basic categories

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  before_save :set_status
+
+  private
+
+  def set_status
+    if Date.today.wday == 6 || Date.today.wday == 0
+      self.status = 'weekly'
+    end
+    if Date.today.day.include?(25..31)
+      self.status = 'monthly'
+    end
+  end
 end
