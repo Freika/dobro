0d97c3b99e9c6216cd3df8369e49d1a3be3a1706
Freika <iamfrey@gmail.com>
2014-12-20 12:59:48 +0600

    Fix elseif in posts_helper.rb

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -8,7 +8,7 @@ module PostsHelper
       content_tag(:span, 'Daily', class: 'label label-primary')
     elsif post.status == 'weekly'
       content_tag(:span, 'Weekly', class: 'label label-info')
-    elseif post.status == 'monthly'
+    elsif post.status == 'monthly'
       content_tag(:span, 'Monthly', class: 'label label-success')
     else
       puts "There is no any status"
