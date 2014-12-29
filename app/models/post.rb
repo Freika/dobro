2616ea10f33ba2b2b7da1ff11760bc43bd6a7c01
Freika <iamfrey@gmail.com>
2014-12-29 12:48:00 +0600

    Fix form checkbox position and helper conditions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   validates :status, inclusion: { in: %w(daily weekly monthly yearly summer winter spring autumn) }
+  validates :content, presence: true
 end
