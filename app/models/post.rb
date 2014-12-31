501066134406571892cdd9fdf6ddda277487b589
Freika <iamfrey@gmail.com>
2014-12-31 21:37:31 +0600

    Commented streak for a while

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,17 +2,17 @@ class Post < ActiveRecord::Base
   belongs_to :user
   validates :status, inclusion: { in: %w(daily weekly monthly yearly summer winter spring autumn) }
   validates :content, presence: true
-  after_save :update_streak
+  # after_save :update_streak
 
 
-  def update_streak
-    user = User.find(self.user_id)
-    if user.posts.last.created_at.to_date == Date.today
-      user.streak += 1
-    else
-      user.streak = 0
-    end
-    user.save
-  end
+  # def update_streak
+  #   user = User.find(self.user_id)
+  #   if user.posts.last.created_at.to_date == Date.today
+  #     user.streak += 1
+  #   else
+  #     user.streak = 0
+  #   end
+  #   user.save
+  # end
 
 end
