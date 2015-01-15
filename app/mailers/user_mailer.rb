661ebeee8167c85a02d4fbb4cdd81448516cac66
Freika <iamfrey@gmail.com>
2015-01-16 00:55:43 +0600

    Fix sending mail to user object

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ApplicationMailer
   def daily_remind(user)
     @user = user
-    mail(to: @user, subject: 'Daily remind')
+    mail(to: @user.email, subject: 'Daily remind')
   end
 end
